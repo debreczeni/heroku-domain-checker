e1b90816bab52438e294e236a69902b3f82e99f9
David Debreczeni <david@octarine-labs.com>
2012-12-17 13:01:15 +0100

    run as a web app

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: ruby worker.rb flag
-save: ruby worker.rb save
+web: bundle exec rackup config.ru -p $PORT
--- worker.rb
+++ worker.rb
@@ -8,20 +8,9 @@ require 'lib/cache'
 require 'lib/hosting_checker'
 require 'models/record'
 require 'models/heroku_address'
-require 'sinatra'
 
 Boot.boot!
 # Boot.clean_db!
 
-get '/' do
-  'Hello world!'
-end
-
 checker = HostingChecker.new# force: true
-
-case ARGV[0]
-when 'flag'
-  checker.flag_domains_hosted_on_heroku
-when 'save'
-  checker.save_top1m_site_in_database
-end
+checker.flag_domains_hosted_on_heroku
